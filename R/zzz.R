a63f15a77b2b2a448564f776ab716121d99fda07
Sebastian Gregoricchio <sebastian.gregoricchio@gmail.com>
2025-09-10 16:25:53 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach =
   function(libname, pkgname){
     msg = paste0("Loading required package: ", pkgname,"\n\n",
-                 "If you are using DEprot in your work please cite:\n",
+                 "If you are using `",pkgname,"` in your work please cite:\n",
                  "'XYZ. XXJournal. 20XX.'")
     packageStartupMessage(msg)
 
@@ -9,17 +9,17 @@
     # Check internet connection
     if (isFALSE(curl::has_internet())) {return(invisible(NULL))}
 
-    # Store DEprot package versions
-    check = suppressMessages(rvcheck::check_github("sebastian-gregoricchio/DEprot"))
+    # Store package versions
+    check = suppressMessages(rvcheck::check_github(paste0("sebastian-gregoricchio/",pkgname)))
     # When up-to-date == NA it means that it is a developmental version higher then the last release
     if (is.na(check$up_to_date)) {return(invisible(NULL))}
 
-    # Check if DEprot is up-to-date and print a message if not up-to-date
+    # Check if package is up-to-date and print a message if not up-to-date
     if (check$up_to_date == F) {
-      message = paste("| The 'DEprot' package is not up-to-date. Installed version v",
+      message = paste("| The `",pkgname,"` package is not up-to-date. Installed version v",
                       check$installed_version, " --> v", check$latest_version,
                       " available.", sep="")
-      command = '| To update DEprot type: devtools::install_github("sebastian-gregoricchio/DEprot", build_manual = T, build_vignettes = T)'
+      command = paste0('| To update ',pkgname,' type: `remotes::install_github("sebastian-gregoricchio/',pkgname,'", build_manual = TRUE, build_vignettes = TRUE)`')
 
       packageStartupMessage(paste("\033[0;37;44m",
                                   paste(rep("-", nchar(command)+2), collapse = ""),
